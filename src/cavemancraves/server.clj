505a951d3bf9fcf5cf62288bf1c3be3ec80b9d79
Øyvind Neshaug <oyvind@neshaug.net>
2012-10-15 12:55:46 +0200

    Prevent dyno idling

--- src/cavemancraves/server.clj
+++ src/cavemancraves/server.clj
@@ -1,11 +1,35 @@
 (ns cavemancraves.server
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server]
+            [cronj.core :as cj])
+  (:import (java.net URL)
+           (java.lang StringBuilder)
+           (java.io BufferedReader InputStreamReader)))
+
+(defn fetch-url
+  [address]
+  (let [url (URL. address)]
+    (with-open [stream (. url openStream)]
+      (let [buf (BufferedReader. (InputStreamReader. stream))]
+        (apply str (line-seq buf))))))
+
+(defn prevent-dyno-idling
+  []
+  (cj/load-tasks!
+   [{:id 1 
+     :desc "Prevent dyno idling" 
+     :handler (fn [job]
+                (println "Preventing dyno idling")
+                (fetch-url "http://cavemancraves.com")) 
+     :tab "1 1,30 * * * * *"}])
+  (cj/start!)
+  (println "Started scheduled job for preventing dyno idling"))
 
 (server/load-views "src/cavemancraves/views/")
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
+    (println "Hello from Caveman Craves! Have a good and healthy day.")
     (server/start port {:mode mode
-                        :ns 'cavemancraves})))
-
+                        :ns 'cavemancraves})
+    (prevent-dyno-idling)))
