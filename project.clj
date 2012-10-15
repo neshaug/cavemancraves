55d6eb2e82c5b2067ac60a0c50f8cc3fccb8a571
Øyvind Neshaug <oyvind@neshaug.net>
2012-10-15 12:53:31 +0200

    Add Cronj 0.3.3

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.1"]
+                           [cronj "0.3.3"]
                            [com.github.twitter/bootstrap "2.1.0"]]
             :repositories [["webjars" "http://webjars.github.com/m2"]]
             :main cavemancraves.server)
